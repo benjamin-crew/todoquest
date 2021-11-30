60a91b3215f418b86e94f9806d884e46666a0ebd
Benjamin Crew <benncrew94@gmail.com>
2021-11-30 07:38:09 +0000

    Classes may not be needed anymore.

--- classes.py
+++ classes.py
@@ -1,14 +1,16 @@
-class Section:
-    """A class to model a section."""
+### This may not be needed anymore.
 
-    def __init__(self, section_title):
-        """Initialise section name and files."""
-        self.section_title = section_title
-        self.file = (f"sections/{section_title}.csv")
-        self.archive = (f"sections/{section_title}_archive.csv")
+# class Section:
+#     """A class to model a section."""
+
+#     def __init__(self, section_title):
+#         """Initialise section name and files."""
+#         self.section_title = section_title
+#         self.file = (f"sections/{section_title}.csv")
+#         self.archive = (f"sections/{section_title}_archive.csv")
     
-    # def create_files(self):
-    #     try:
-    #         with open(f"sections/{section_title}.csv")
-    #     except FileNotFoundError:
-    #         with 
+#     # def create_files(self):
+#     #     try:
+#     #         with open(f"sections/{section_title}.csv")
+#     #     except FileNotFoundError:
+#     #         with 
